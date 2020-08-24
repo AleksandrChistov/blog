bc345f19027dc4afd06553b72621e0feb7724a4a
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2020-08-24 09:06:27 +0700

    Added delete method

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchStr = '';
 
   constructor(private postsService: PostsService) { }
@@ -22,13 +23,19 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     })
   }
 
+  delete(id: string) {
+    this.dSub = this.postsService.delete(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
+  }
+
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
-  }
 
-  remove(id: string) {
-
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 }
